34f17a3aee1aec16e4d0cc4f6012639a1cad3df3
rielisson <118862671+rielisson@users.noreply.github.com>
2026-05-08 20:38:03 -0300

    feat: Login passando name para Dashboard e ajustes de layout

--- src/componentes/Dashboard.tsx
+++ src/componentes/Dashboard.tsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
 import { Header } from "./Header";
 
+interface DashboardProps {
+    name: string;
+}
 
-export function Dashboard(name: string) {
-    const [userName, setUserName] = useState("");
-    setUserName(name);
+export function Dashboard({name}: DashboardProps)  {
     return (
         <div className="min-h-screen bg-white">
             <Header />
             <div className="w-full max-w-md">
-                <div className="bg-white">
-                    <h1>Bem vindo {userName}</h1>
+                <div className="w-max h-8 flex flex-col mt-15 ml-20">
+                    <h1 className="text-center mb-2 text-3xl font-bold">Bem vindo {name}!  👋 </h1>
+                    <p className="text-gray-900">Continue sua jornada de aprendizado</p>
                 </div>
             </div>
         </div>
